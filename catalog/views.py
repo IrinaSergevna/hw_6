66a76790e09537b007c1622d2a7cc84dd1e79d25
IrinaSergevna <ir.saldaeva@gmail.com>
2024-11-01 16:16:41 +0300

    Дополнительное задание

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def home(request):
-    return render(request, "home.html")
-
+    latest_products = Product.objects.order_by('-created_at')[:5]
+    print("Последние товары:", latest_products)
+    return render(request, "home.html", {'latest_products': latest_products})
 
 def contact(request):
     if request.method == "POST":
@@ -12,3 +14,7 @@ def contact(request):
         message = request.POST.get("message")
         print(f"{name} ({email}): {message}")
     return render(request, "contact.html")
+
+
+
+
